10001dce0293487ff959c380adf926236dbb1880
Ron Trompert <ron.trompert@surf.nl>
2024-08-23 16:17:10 +0200

    Ton of bugs fixed

--- bigfile_upload.py
+++ bigfile_upload.py
@@ -11,15 +11,17 @@ from queue import Queue
 import concurrent.futures
 
 
-mypath='/moggûh/burp/aap/noot/mies/'
-file='test3.jpg'
+mypath='/path/to/folder/in/nextcloud'
+file='my_file_to_upload'
 
-block_size=50000
-user_name=''
-password=''
-host=''
+block_size=50000000
+
+user_name='' #Put your username here
+password=''  #Put your password here
+host=''      #Put your Nextcloud host here
+
 url='https://'+host+'/remote.php/dav'
-concurrent_uploads=10
+concurrent_uploads=7
 
 url=url.rstrip('/')
 mypath=mypath.strip('/')
@@ -28,9 +30,10 @@ uploads=url+'/uploads/'+user_name
 path_url=url+'/files/'+user_name
 
 # This is the maximum defined by Nextcloud
+# Do not change!!!
 max_chunks=10000
 
-tmpdir='bigfileupload'+str(uuid.uuid4())
+tmpdir='bigfileupload-'+str(uuid.uuid4())
 
 def create_dir(url,dirlist):
 
@@ -83,21 +86,27 @@ def upload(filename,q,sz,upl,dst):
         number=filepart[0]
         offset=filepart[1]
         chunk=filepart[2]
+        print("Uploading chunk "+number+" of "+str(chunk)+" bytes")
         r=requests.put(upl+'/'+number,auth=auth,headers=headers,data=read_in_chunk(f,offset,chunk))
         if not ( r.status_code>=200 and r.status_code<300 ):
             print("Unable to upload "+filename,file=sys.stderr)
             print("Error code: "+str(r.status_code),file=sys.stderr)
+            q.task_done()
             sys.exit(2)
+        else:
+            print("Chunk "+number+" successfully uploaded")
+        q.task_done()
 
     f.close()
 
 def move_to_destination(sz,upl,dst):
     headers={'OC-Total-Length':str(sz),'Destination':dst}
     auth=(user_name,password)
-    r=requests.request('MOVE',upl+'/.file',auth=(user_name,password), headers=headers)
+    r=requests.request('MOVE',upl+'/.file',auth=(user_name,password), headers=headers,timeout=7200)
     if not ( r.status_code>=200 and r.status_code<300 ):
         print("Unable to move "+upl,file=sys.stderr)
         print("Error code: "+str(r.status_code),file=sys.stderr)
+        print(r.content)
         sys.exit(2)
 
 if __name__ == '__main__':
@@ -127,4 +136,6 @@ if __name__ == '__main__':
         for i in range(concurrent_uploads):
             executor.submit(upload,file,q,filesize,upload_url,destination_url)
 
+    q.join()
+    print("Assembling the chunks and copy the data to :"+destination_url)
     move_to_destination(filesize,upload_url,destination_url)
